78ddaa1645fa9485d3a721fea6479e199c8258a2
Dipali Trivedi <dipali.trivedi@pearson.com>
2009-04-13 17:44:37 +0000

    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@6039 0b2df0ae-bbdc-4912-b7de-57517db36531

---
+++ src/main/java/com/pearson/ed/lplc/model/validation/LicensePoolValidator.java
@@ -0,0 +1,65 @@
+
+package com.pearson.ed.lplc.model.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.pearson.ed.lplc.common.LPLCConstants;
+import com.pearson.ed.lplc.dto.LicensePoolDTO;
+
+
+public class LicensePoolValidator implements Validator {
+
+	private final Class<?>[] validatedTypes = new Class<?>[] { LicensePoolDTO.class };
+	private List<String> validationErrors = new ArrayList<String>();
+	private LicensePoolDTO licensepool;
+
+	/**
+	 * Returns an array of valid types that LicensePoolValidator can validate.
+	 * LicensePoolValidator is current designed to validate LicensePool and any of its
+	 * subclasses.
+	 * 
+	 * @return an array of classes the validator supports.
+	 */
+	public Class<?>[] getValidatedTypes() {
+		return validatedTypes;
+	}
+
+	/**
+	 * Constructs a list of error that occurred during validation.
+	 * 
+	 * @return a list of errors.
+	 */
+	public List<String> getValidationErrors() {
+		return validationErrors;
+	}
+
+	/**
+	 * Performs validation on the domain object. The method checks to see that
+	 * the object is an instance of a class supported by the validator (i.e.
+	 * User, and then checks that the ID and password are not empty.
+	 * 
+	 * @return true if the object passes validation.
+	 */
+	public boolean validate(Object domainObject) {
+		boolean result = true;
+		if (!(domainObject instanceof LicensePoolDTO)) {
+			throw new IllegalArgumentException("Invalid parameters.");
+		}
+		licensepool = (LicensePoolDTO) domainObject;
+
+		String mode = licensepool.getMode();
+
+		if (LPLCConstants.CREATE_MODE.equals(mode)) {
+			if (licensepool.getStartDate()==null) {
+				result = false;
+				validationErrors.add("LicensePool Start Date must not be empty");
+			}
+		}
+
+		return result;
+	}
+
+}
---
+++ src/main/java/com/pearson/ed/lplc/model/validation/Validator.java
@@ -0,0 +1,47 @@
+/**-----------------------------------------------------------------------------
+ | File Name: Validator.java
+ | Create Date: Jun 4, 2008
+ | @author: UHERWYU                                                             
+ |                                                                              
+ | COPYRIGHT © 2008 Pearson Inc.   
+ | -----------------------------------------------------------------------------
+ |                                                                              
+ | History:                                                                     
+ |==============================================================================
+ | Flags  Date      AUTHOR    Comments                                          
+ | -----------------------------------------------------------------------------
+ | YH01                                                                  
+ \*-----------------------------------------------------------------------------*/
+package com.pearson.ed.lplc.model.validation;
+
+import java.util.List;
+
+/**
+ * Validator is the interface that all model validator should implement.
+ * 
+ */
+
+public interface Validator {
+	/**
+	 * Performs validation on the domain objects. An object passes validation
+	 * if it obeys all of the business constraint rules.
+	 * 
+	 * @return true if the object passes validation.
+	 */
+	boolean validate(Object domainObject);
+	
+	/**
+	 * Creates a list of errors that occurred during the most recent call to
+	 * validate.
+	 * 
+	 * @return a list of errors that occurred during the last validation.
+	 */
+	List <String> getValidationErrors();
+	
+	/**
+	 * Creates an array of Classes which can be validated.
+	 * 
+	 * @return an array of Classes available for validation.
+	 */
+	Class[] getValidatedTypes();
+}
