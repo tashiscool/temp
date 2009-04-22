c3ee7abe4354c4eccac5b286e4d6e93f50f083ed
Dipali Trivedi <dipali.trivedi@pearson.com>
2009-04-22 00:11:45 +0000

    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@6220 0b2df0ae-bbdc-4912-b7de-57517db36531

---
+++ src/main/java/com/pearson/ed/lplc/ws/LicensePoolWebServiceConstants.java
@@ -0,0 +1,19 @@
+package com.pearson.ed.lplc.ws;
+
+public interface LicensePoolWebServiceConstants {
+	
+	/**
+	 * Create licensepool request element.
+	 */
+	String CREATE_LICENSEPOOL_REQUEST_ELEMENT = "CreateLicensePoolRequest";
+	
+	/**
+	 * Update licensepool request element.
+	 */
+	String UPDATE_LICENSEPOOL_REQUEST_ELEMENT = "UpdateLicensePoolRequest";
+	
+	/**
+	 * The licensepool namespace.
+	 */
+	String LICENSEPOOL_NAMESPACE = "http://licensepool.rws.pearson.com/doc/2009/04/01/";
+}
