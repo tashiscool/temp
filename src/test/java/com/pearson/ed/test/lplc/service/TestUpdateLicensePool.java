d5828f2d3090e99061d38f3541d6c444f33ede01
Dipali Trivedi <dipali.trivedi@pearson.com>
2009-04-23 17:01:31 +0000

    Added unit tests for updatelicensepool service.
    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@6270 0b2df0ae-bbdc-4912-b7de-57517db36531

---
+++ src/test/java/com/pearson/ed/test/lplc/service/TestUpdateLicensePool.java
@@ -0,0 +1,66 @@
+package com.pearson.ed.test.lplc.service;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+import com.pearson.ed.lplc.dao.api.LicensePoolDAO;
+import com.pearson.ed.lplc.dto.LicensePoolDTO;
+import com.pearson.ed.lplc.dto.UpdateLicensePoolDTO;
+import com.pearson.ed.lplc.model.LicensePoolMapping;
+import com.pearson.ed.lplc.services.api.LicensePoolService;
+import com.pearson.ed.test.lplc.common.BaseIntegrationTest;
+
+@RunWith(JUnit4ClassRunner.class)
+public class TestUpdateLicensePool extends BaseIntegrationTest {
+
+	@Test
+	public void testUpdateLicensePool() {
+		LicensePoolService licensepoolService = loadLicensePoolService();
+		LicensePoolDTO licensepool = loadLicensePool();
+		LicensePoolDAO licensepoolDAO = loadLicensePoolDAO();
+		String licensepoolId = licensepoolService
+				.createLicensePool(licensepool);
+		UpdateLicensePoolDTO updateDTO = new UpdateLicensePoolDTO();
+		updateDTO.setLicensepoolId(licensepoolId);
+		updateDTO.setQuantity(500);
+		String updateLicensePool = licensepoolService
+				.updateLicensePool(updateDTO);
+		LicensePoolMapping findByLicensePoolId = licensepoolDAO
+				.findByLicensePoolId(updateLicensePool);
+		assertEquals(500, findByLicensePoolId.getQuantity());
+	}
+
+	@Test
+	public void testUpdateLicensePoolForStartDateEndDate() {
+		LicensePoolService licensepoolService = loadLicensePoolService();
+		LicensePoolDTO licensepool = loadLicensePool();
+		String licensepoolId = licensepoolService
+				.createLicensePool(licensepool);
+		UpdateLicensePoolDTO updateDTO = new UpdateLicensePoolDTO();
+		updateDTO.setLicensepoolId(licensepoolId);
+		updateDTO.setStartDate(new Date());
+		try{
+			licensepoolService.updateLicensePool(updateDTO);
+		}
+		catch(Exception e){
+			assertEquals("Start Date can not be greater than End Date", e.getMessage());
+		}
+	}
+
+	public LicensePoolService loadLicensePoolService() {
+		return (LicensePoolService) applicationContext
+				.getBean("licensepoolService");
+	}
+
+	public LicensePoolDTO loadLicensePool() {
+		return (LicensePoolDTO) applicationContext
+				.getBean("serviceTestLicensePool");
+	}
+
+	public LicensePoolDAO loadLicensePoolDAO() {
+		return (LicensePoolDAO) applicationContext.getBean("licensepoolDAO");
+	}
+}
