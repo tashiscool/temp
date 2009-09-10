1e617f5ed47ead09d835d6324467d475b8091268
Lakshmi Dasari <Lakshmi.Dasari@pearson.com>
2009-09-10 18:14:38 +0000

    US6041:Peer review remediations. Added new Stub implementation for bypassing real Organization integration.
    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@10189 0b2df0ae-bbdc-4912-b7de-57517db36531

---
+++ src/test/java/com/pearson/ed/lplc/stub/impl/OrganizationServiceClientStubImpl.java
@@ -0,0 +1,21 @@
+package com.pearson.ed.lplc.stub.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pearson.ed.lplc.stub.api.OrganizationServiceClient;
+import com.pearson.ed.lplc.stub.dto.OrganizationDTO;
+
+public class OrganizationServiceClientStubImpl implements OrganizationServiceClient{
+	@Override
+	public List<OrganizationDTO> getChildOrganizations(String organizationId) {
+		List<OrganizationDTO> orgList = new ArrayList<OrganizationDTO>();
+		orgList.add(new OrganizationDTO("DummyChildOrg1",1));
+		orgList.add(new OrganizationDTO("DummyChildOrg2",1));
+		orgList.add(new OrganizationDTO("DummyChildOrg3",2));
+		orgList.add(new OrganizationDTO("DummyChildOrg4",2));
+		orgList.add(new OrganizationDTO("DummyChildOrg5",2));
+		orgList.add(new OrganizationDTO("DummyChildOrg6",3));
+		return orgList;
+	}		
+}
