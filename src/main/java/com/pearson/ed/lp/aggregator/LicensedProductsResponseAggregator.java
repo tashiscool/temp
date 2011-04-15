37a6f9bd2ff4ff167445523eb62dd8792151dfeb
Nicholas Lloyd <nicholas.lloyd@pearson.com>
2011-04-15 19:40:16 +0000

    US33039 changes - implemented last aggregator with only one setter function remaining - added ProductData pojo nested within ProductEntityIdsResponse
    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@23771 0b2df0ae-bbdc-4912-b7de-57517db36531

--- src/main/java/com/pearson/ed/lp/aggregator/LicensedProductsResponseAggregator.java
+++ src/main/java/com/pearson/ed/lp/aggregator/LicensedProductsResponseAggregator.java
@@ -1,12 +1,25 @@
 package com.pearson.ed.lp.aggregator;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.annotation.Aggregator;
 
+import com.pearson.ed.lp.message.LicensedProductDataCollection;
+import com.pearson.ed.lp.message.OrderLineItemsResponse;
+import com.pearson.ed.lp.message.OrganizationDisplayNamesResponse;
+import com.pearson.ed.lp.message.ProductEntityIdsResponse;
+import com.pearson.ed.lp.message.ProductEntityIdsResponse.ProductData;
+import com.pearson.ed.lplc.model.OrganizationLPMapping;
 import com.pearson.rws.licensedproduct.doc.v2.GetLicensedProductResponseElement;
+import com.pearson.rws.licensedproduct.doc.v2.LicensedProduct;
 
 public class LicensedProductsResponseAggregator {
 	
@@ -19,8 +32,88 @@ public class LicensedProductsResponseAggregator {
 					responseMessages.size()));
 			LOGGER.debug(responseMessages.toString());
 		}
-		// TODO implementation
-		return new GetLicensedProductResponseElement();
+		
+		ProductEntityIdsResponse productsData = null;
+		OrderLineItemsResponse orderData = null;
+		LicensedProductDataCollection licensePoolData = null;
+		OrganizationDisplayNamesResponse organizationData = null;
+		
+		for(Object responseMessage : responseMessages) {
+			if(responseMessage instanceof ProductEntityIdsResponse) {
+				productsData = (ProductEntityIdsResponse)responseMessage;
+			} else if(responseMessage instanceof OrderLineItemsResponse) {
+				orderData = (OrderLineItemsResponse)responseMessage;
+			} else if(responseMessage instanceof LicensedProductDataCollection) {
+				licensePoolData = (LicensedProductDataCollection)responseMessage;
+				organizationData = licensePoolData.getOrganizationDisplayNames();
+			}
+		}
+		
+		// TODO null checks?
+		
+		GetLicensedProductResponseElement getLicensedProductResponse = new GetLicensedProductResponseElement();
+		List<LicensedProduct> licensedProducts = getLicensedProductResponse.getLicensedProduct();
+
+		for(OrganizationLPMapping licensePool : licensePoolData.getLicensePools().getLicensePools()) {
+			LicensedProduct licensedProduct = new LicensedProduct();
+			licensedProducts.add(licensedProduct);
+			
+//			<ns:LicensedProduct>
+//		    <ns:OrganizationId>{data($OrganizationId)}</ns:OrganizationId>
+//		    <ns:LicensePoolId>{data($LicensePoolByOrganizationId[$index]/ns1:LicensePoolId)}</ns:LicensePoolId>
+//		    <ns:LicensedOrganizationId>{data($OrganizationId)}</ns:LicensedOrganizationId>
+//		    <ns:LicensedOrganizationDisplayName>{data($LicensedOrganizationDisplayName)}</ns:LicensedOrganizationDisplayName>
+//		    <ns:LicensePoolType>{data($LicensePoolByOrganizationId[$index]/ns1:Type)}</ns:LicensePoolType>
+//		    <ns:LicensePoolStatus>{data($LicensePoolByOrganizationId[$index]/ns1:Status)}</ns:LicensePoolStatus>
+//		    <ns:DenyNewSubscription>{data($LicensePoolByOrganizationId[$index]/ns1:DenyNewSubscription)}</ns:DenyNewSubscription>
+//		    <ns:StartDate>{data($LicensePoolByOrganizationId[$index]/ns1:StartDate)}</ns:StartDate>
+//		    <ns:EndDate>{data($LicensePoolByOrganizationId[$index]/ns1:EndDate)}</ns:EndDate>
+//		    <ns:Quantity>{data($LicensePoolByOrganizationId[$index]/ns1:Quantity)}</ns:Quantity>
+//		    <ns:UsedLicenses>{data($LicensePoolByOrganizationId[$index]/ns1:UsedLicenses)}</ns:UsedLicenses>
+//		    <ns:ProductId>{data($productDetails//ProductId)}</ns:ProductId>
+//		    <ns:ProductDisplayName>{data($productDetails//ProductDisplayName)}</ns:ProductDisplayName>
+//		    <ns:CGProgram>{data($productDetails//CGAttribute)}</ns:CGProgram>
+//		    <ns:OrderedISBN>{data($GetOrderLineItemByIdResponse/ns6:Order/ns6:OrderLineItems/ns6:OrderLine/ns6:OrderedISBN)}</ns:OrderedISBN>
+		// </ns:LicensedProduct>
+			
+			String organizationId = licensePool.getLicensepoolMapping().getOrg_id();
+			Long productEntityId = Long.valueOf(licensePool.getLicensepoolMapping().getProduct_id());
+			ProductData productData = productsData.getProductDataByEntityIds().get(productEntityId);
+			String firstOrderLineItemId = licensePool.getLicensepoolMapping()
+				.getOrderLineItems().iterator().next().getOrderLineItemId();
+
+			licensedProduct.setOrganizationId(organizationId);
+			licensedProduct.setLicensePoolId(licensePool.getLicensepoolMapping().getLicensepoolId());
+			licensedProduct.setLicensedOrganizationId(organizationId);
+			licensedProduct.setLicensedOrganizationDisplayName(
+					organizationData.getOrganizationDisplayNamesByIds().get(organizationId));
+			licensedProduct.setLicensePoolType(licensePool.getLicensepoolMapping().getType());
+			licensedProduct.setLicensePoolStatus(licensePool.getLicensepoolMapping().getStatus());
+			licensedProduct.setDenyNewSubscription(licensePool.getLicensepoolMapping().getDenyManualSubscription());
+			
+			try {
+				licensedProduct.setStartDate(convertToXMLGregorianCalendar(licensePool.getLicensepoolMapping().getStart_date()));
+				licensedProduct.setEndDate(convertToXMLGregorianCalendar(licensePool.getLicensepoolMapping().getEnd_date()));
+			} catch (DatatypeConfigurationException e) {
+				LOGGER.error(e.getMessage());
+			}
+			
+			licensedProduct.setQuantity(licensePool.getLicensepoolMapping().getQuantity());
+			licensedProduct.setUsedLicenses(licensePool.getUsed_quantity());
+			licensedProduct.setProductId(productEntityId.toString());
+			licensedProduct.setProductDisplayName(productData.getDisplayName());
+			licensedProduct.setCGProgram(productData.getCgAttribute());
+//			licensedProduct.getGradeLevel().add( TODO
+			licensedProduct.setOrderedISBN(orderData.getOrderedISBNsByOrderLineItemIds().get(firstOrderLineItemId));
+		}
+		
+		return getLicensedProductResponse;
+	}
+	
+	private XMLGregorianCalendar convertToXMLGregorianCalendar(Date date) throws DatatypeConfigurationException {
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		gregorianCalendar.setTime(date);
+		return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
 	}
 	
 }
--- src/main/java/com/pearson/ed/lp/message/ProductEntityIdsResponse.java
+++ src/main/java/com/pearson/ed/lp/message/ProductEntityIdsResponse.java
@@ -5,29 +5,65 @@ import java.util.Map;
 
 /**
  * Response for the ProductLifeCycleClient api which wraps a map
- * of Product Entity Id values to Product Display Name strings.
+ * of Product Entity Id values to ProductData instances which
+ * are simple POJOs containing all necessary product data for the
+ * GetLicensedProduct service response.
  * 
  * @author ULLOYNI
  *
  */
 public class ProductEntityIdsResponse {
 	
-	private Map<Long,String> productDisplayNamesByEntityIds = new Hashtable<Long,String>();
+	private Map<Long,ProductData> productDataByEntityIds = new Hashtable<Long,ProductData>();
 
 	/**
-	 * Get the map of Product Entity Id values to Product Display Name strings.
+	 * Get the map of Product Entity Id values to Product data collections.
 	 * @return
 	 */
-	public Map<Long, String> getProductDisplayNamesByEntityIds() {
-		return productDisplayNamesByEntityIds;
+	public Map<Long, ProductData> getProductDataByEntityIds() {
+		return productDataByEntityIds;
 	}
 
 	/**
-	 * Set the map of Product Entity Id values to Product Display Name strings.
-	 * @param productDisplayNamesByEntityIds
+	 * Set the map of Product Entity Id values to Product data collections.
+	 * @param productDataByEntityIds
 	 */
-	public void setProductDisplayNamesByEntityIds(Map<Long, String> productDisplayNamesByEntityIds) {
-		this.productDisplayNamesByEntityIds = productDisplayNamesByEntityIds;
+	public void setProductDataByEntityIds(Map<Long, ProductData> productDataByEntityIds) {
+		this.productDataByEntityIds = productDataByEntityIds;
+	}
+	
+	/**
+	 * Simple pojo for holding product data.
+	 * 
+	 * @author ULLOYNI
+	 *
+	 */
+	public class ProductData {
+		
+		private String displayName;
+		
+		private String cgAttribute;
+		
+		private String gradeLevel;
+		
+		public String getDisplayName() {
+			return displayName;
+		}
+		public void setDisplayName(String displayName) {
+			this.displayName = displayName;
+		}
+		public String getCgAttribute() {
+			return cgAttribute;
+		}
+		public void setCgAttribute(String cgAttribute) {
+			this.cgAttribute = cgAttribute;
+		}
+		public String getGradeLevel() {
+			return gradeLevel;
+		}
+		public void setGradeLevel(String gradeLevel) {
+			this.gradeLevel = gradeLevel;
+		}
 	}
 
 }
