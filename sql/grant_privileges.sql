93fa614e3c5b1d2efb00bc6f18184208c7ef6021
Brian Lalor <brian.lalor@pearson.com>
2011-07-14 15:24:55 +0000

    US36631 changes - CAS properties added and modified to fit rumba.local.properties - CAS project properties reference rumba.properties on classpath and property placeholder names updated accordingly in xml config files - TODO in CAS: need to disable filtering of *.xml files in src/main/webapp... this is still happening and I am not sure why - changes across the board in pom.xml files to support sql script filtering using rumba.${env}.properties files, included updating profiles in each webservice (though can be deleted in place of profiles in webservices parent pom) - fixed minor issues in bulk and organization life cycle services causing deployment failures in tomcat
    US36632 changes
    - tomcat bundle include CAS war
    
    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@25328 0b2df0ae-bbdc-4912-b7de-57517db36531

--- sql/grant_privileges.sql
+++ sql/grant_privileges.sql
@@ -3,17 +3,15 @@
 BEGIN
 
 -- TABLES
-EXECUTE IMMEDIATE 'grant select,insert,update,delete ON LICENSEPOOL to @user@';
-EXECUTE IMMEDIATE 'grant select,insert,update,delete ON LICENSEPOOL_ORDERLINEITEM to @user@';
-EXECUTE IMMEDIATE 'grant select,insert,update,delete ON LICENSEPOOL_ORGANIZATION to @user@';
+EXECUTE IMMEDIATE 'grant select,insert,update,delete ON LICENSEPOOL to @licensepool.hibernate.connection.app.username@';
+EXECUTE IMMEDIATE 'grant select,insert,update,delete ON LICENSEPOOL_ORDERLINEITEM to @licensepool.hibernate.connection.app.username@';
+EXECUTE IMMEDIATE 'grant select,insert,update,delete ON LICENSEPOOL_ORGANIZATION to @licensepool.hibernate.connection.app.username@';
 
 -- grants for solr
 EXECUTE IMMEDIATE 'grant select ON licensepool               to @solr.user@';
 EXECUTE IMMEDIATE 'grant select ON licensepool_orderlineitem to @solr.user@';
 
-
--- FUNCTIONS
-EXECUTE IMMEDIATE 'grant execute ON COMPUTE_STATUS_LICENSEPOOL to @user@';
+EXECUTE IMMEDIATE 'grant execute ON COMPUTE_STATUS_LICENSEPOOL to @licensepool.hibernate.connection.app.username@';
 
 END;
 
