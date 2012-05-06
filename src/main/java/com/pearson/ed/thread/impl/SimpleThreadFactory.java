e904fd40300553f932e017a5925b4dca10eba4ad
Tashdid Khan <tashdid.khan@pearson.com>
2012-05-06 09:18:43 +0000

    updating for executors to have daemons that will terminate on termination of program
    git-svn-id: file:///fs/ulcdc/ulcdev/blalor/svnsync/common_services/main/trunk/application_src/webservices/licensepool@36436 0b2df0ae-bbdc-4912-b7de-57517db36531

---
+++ src/main/java/com/pearson/ed/thread/impl/SimpleThreadFactory.java
@@ -0,0 +1,12 @@
+package com.pearson.ed.thread.impl;
+import java.util.concurrent.ThreadFactory;
+import java.lang.Thread;
+import java.lang.Runnable;
+
+public class SimpleThreadFactory implements ThreadFactory {
+	public Thread newThread(Runnable r) {
+		Thread thread = new Thread(r);
+		thread.setDaemon(true);
+		return thread;
+	}
+}
